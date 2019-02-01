1642bbede2211e24f7a7323103cb4e5c0db1c838
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2019-02-01 17:36:12 +0530

    :sparkles: render navbar and jobs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './Navbar';
+import Jobs from './Jobs';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      jobs: []
+    }
+  }
+
+  fetchJobs = () => {
+    fetch('https://jobs.github.com/positions.json?markdown=true')
+    .then(res => res.json())
+    .then(jobs => this.setState({jobs}))
+  }
+
+  componentWillMount(){
+    this.fetchJobs()
+  }
+
+  createdDate = (date) => {
+    let postedDate = date.split(' ')
+    return `${postedDate[0]}, ${postedDate[2]} ${postedDate[1]}, ${postedDate[5]}`
+  }
+
   render() {
+    const { jobs } = this.state
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Navbar /> <br/>
+        <div className="container">
+          {jobs && jobs.map(job => (
+            <Jobs 
+            id={job.id}
+            key={job.id}
+            title={job.title}
+            desc={job.description}
+            company={job.company} 
+            company_url={job.company_url}
+            created_at={this.createdDate(job.created_at)}
+            company_logo={job.company_logo}
+            apply={job.how_to_apply}
+            location={job.location}
+            type={job.type}
+            />
+          ))}
+        </div>
       </div>
     );
   }
