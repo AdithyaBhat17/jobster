c8950dc0a6ef5073e1dcb0d059ac672a33d79477
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2019-02-03 20:03:23 +0530

    :zap: search bar is now functional

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Navbar(){
+export default function Navbar(props){
     return(
         <nav className="navbar navbar-expand-lg navbar-light">
             <div className="container">
@@ -12,7 +12,7 @@ export default function Navbar(){
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ml-auto">
                         <li className="nav-item active">
-                            <input className="searchbar form-control" type="text" placeholder="🔎 search for jobs"/>
+                            <input onChange={props.searchJob} className="searchbar form-control" type="text" placeholder="🔎 search for jobs"/>
                         </li>
                     </ul>  
                 </div>
